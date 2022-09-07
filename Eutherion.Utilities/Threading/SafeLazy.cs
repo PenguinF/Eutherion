8167e91d8941d163fbaed9b9862961f710ea5c06
Henk Nicolai <PenguinF@users.noreply.github.com>
2022-09-07 21:05:50 +0200

    Add SafeLazy<TValue> and SafeLazyObject<TObject> to Eutherion.Threading.

---
+++ Eutherion.Utilities/Threading/SafeLazy.cs
@@ -0,0 +1,102 @@
+﻿#region License
+/*********************************************************************************
+ * SafeLazy.cs
+ *
+ * Copyright (c) 2004-2022 Henk Nicolai
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+**********************************************************************************/
+#endregion
+
+#if !NET472
+#nullable enable
+#endif
+
+using System;
+
+namespace Eutherion.Threading
+{
+    /// <summary>
+    /// Represents the thread-safe lazy evaluation of a parameterless function.
+    /// </summary>
+    /// <typeparam name="TValue">
+    /// The type of the evaluated value.
+    /// </typeparam>
+    public class SafeLazy<TValue>
+    {
+        private readonly object Sentinel;
+#if NET472
+        private Func<TValue> Func;
+#else
+        private Func<TValue>? Func;
+#endif
+        private TValue EvaluatedValue;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="SafeLazy{TValue}"/>.
+        /// </summary>
+        /// <param name="func">
+        /// The function to evaluate when the <see cref="Value"/> property is first accessed.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="func"/> is null.
+        /// </exception>
+        /// <remarks>
+        /// N.B.: The provided function is evaluated while holding an internal lock.
+        /// </remarks>
+        public SafeLazy(Func<TValue> func)
+        {
+            Func = func ?? throw new ArgumentNullException(nameof(func));
+            Sentinel = new object();
+
+#if !NET472
+            // Use the bang operator, as EvaluatedValue is guaranteed to be initialized after calling 'Value'.
+            EvaluatedValue = default!;
+#endif
+        }
+
+        /// <summary>
+        /// Gets the result of the evaluation of the function.
+        /// The function is evaluated only once, when it is first needed.
+        /// </summary>
+        public TValue Value
+        {
+            get
+            {
+                // Use well known pattern for implementing thread safety.
+                // It avoids taking a lock in most cases when Func was already null.
+                if (Func != null)
+                {
+                    lock (Sentinel)
+                    {
+                        // Check Func again because multiple threads may have been racing for the lock.
+                        if (Func != null)
+                        {
+                            // Evaluate the function inside the lock so any other threads
+                            // racing for the evaluated value must wait until it's assigned.
+                            EvaluatedValue = Func();
+
+                            // If Func() threw, then it will be re-evaluated by the next caller,
+                            // and it will likely throw again.
+                            // This also releases any references a closure may have held implicitly.
+                            Func = null;
+                        }
+                    }
+                }
+
+                return EvaluatedValue;
+            }
+        }
+    }
+}
---
+++ Eutherion.Utilities/Threading/SafeLazyObject.cs
@@ -0,0 +1,127 @@
+﻿#region License
+/*********************************************************************************
+ * SafeLazyObject.cs
+ *
+ * Copyright (c) 2004-2022 Henk Nicolai
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+**********************************************************************************/
+#endregion
+
+#if !NET472
+#nullable enable
+#endif
+
+using System;
+using System.Threading;
+
+namespace Eutherion.Threading
+{
+    /// <summary>
+    /// Represents the thread-safe lazy evaluation of a parameterless constructor which creates an object.
+    /// </summary>
+    /// <typeparam name="TObject">
+    /// The type of object to create.
+    /// </typeparam>
+    /// <remarks>
+    /// This class uses the race-to-initialize pattern. This guarantees that <see cref="Object"/> always returns the same object,
+    /// but it is possible that multiple threads race and evaluate the constructor function, which should therefore be cheap.
+    /// </remarks>
+    public struct SafeLazyObject<TObject> where TObject : class
+    {
+#if NET472
+        private volatile Func<TObject> Constructor;
+        private TObject ConstructedObject;
+#else
+        private volatile Func<TObject>? Constructor;
+        private TObject? ConstructedObject;
+#endif
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="SafeLazyObject{TValue}"/>.
+        /// </summary>
+        /// <param name="constructor">
+        /// The object constructor. It must not return null.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="constructor"/> is null.
+        /// </exception>
+        public SafeLazyObject(Func<TObject> constructor)
+        {
+            Constructor = constructor ?? throw new ArgumentNullException(nameof(constructor));
+            ConstructedObject = null;
+        }
+
+        /// <summary>
+        /// Gets the result of the constructor.
+        /// Note that if multiple threads race to construct the object, they will all call the constructor.
+        /// Only one of the created objects is kept, and henceforth returned.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">
+        /// <see cref="Constructor"/> returned null.
+        /// </exception>
+        public TObject Object
+        {
+            get
+            {
+                // Avoid calling the constructor and CompareExchange() if ConstructedObject is already available.
+                if (ConstructedObject == null)
+                {
+                    // Create a local copy of Constructor, so that if another thread succeeds in setting ConstructedObject 
+                    // and also resetting the Constructor field before this thread reaches the CompareExchange statement,
+                    // it will not generate a NullReferenceException. The object created on this thread is then discarded.
+#if NET472
+                    Func<TObject> constructor = Constructor;
+#else
+                    Func<TObject>? constructor = Constructor;
+#endif
+
+                    // Perform CompareExchange on the ConstructedObject rather than the constructor, otherwise threads
+                    // that lost the race might return a null ConstructedObject while the winning thread is still busy
+                    // constructing it.
+                    // If constructor() throws, then it will be re-evaluated by the next caller, and throw again.
+                    if (constructor != null)
+                    {
+                        TObject constructedObject = constructor();
+
+                        // Validate that constructor() returned a non-null value.
+                        if (constructedObject == null)
+                        {
+                            throw new InvalidOperationException(
+                                $"{nameof(Constructor)} returned null but should have returned an initialized object.");
+                        }
+
+                        if (Interlocked.CompareExchange(ref ConstructedObject, constructedObject, null) == null)
+                        {
+                            // For a brief interval here, both Constructor and ConstructedObject != null, which allows
+                            // a different thread to evaluate the constructor.
+
+                            // Setting the constructor to null releases any references the constructor may have held implicitly, had it been a closure.
+                            Constructor = null;
+                        }
+                    }
+                    else if (ConstructedObject == null)
+                    {
+                        // The Constructor field can only be null after ConstructedObject has been set to a non-null value,
+                        // but the static code analyzer cannot see this.
+                        // So this is dead code, and can throw an UnreachableException here to close off this branch to the code analyzer.
+                        throw new UnreachableException();
+                    }
+                }
+
+                return ConstructedObject;
+            }
+        }
+    }
+}
