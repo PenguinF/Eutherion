d5f8822210765dfa7234c717b5d4b0cf72d44adb
Henk Nicolai <PenguinF@users.noreply.github.com>
2023-08-17 20:43:37 +0200

    Breaking: move Box to Eutherion.Utilities.dll and Eutherion namespace.

--- Eutherion.Utilities/Box.cs
+++ Eutherion.Utilities/Box.cs
@@ -2,7 +2,7 @@
 /*********************************************************************************
  * Box.cs
  *
- * Copyright (c) 2004-2022 Henk Nicolai
+ * Copyright (c) 2004-2023 Henk Nicolai
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@
 **********************************************************************************/
 #endregion
 
-namespace System
+namespace Eutherion
 {
     /// <summary>
     /// References a value, adding an indirection.
